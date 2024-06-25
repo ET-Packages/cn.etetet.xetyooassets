96882438503e7e5c4e2fed804229e6e1b5bd331a
IcePower <icepower@git.com>
2024-06-25 21:37:01 +0800

    XET.YooAsset 改为 ET.YooAsset 的扩展，而不是替代。

--- Scripts/Model/Share/CoroutineLockType.cs
+++
@@ -1,8 +0,0 @@
-namespace ET
-{
-    public static partial class CoroutineLockType
-    {
-        public const int Resources = PackageType.XETYooAssets * 1000 + 1;
-        public const int ResourcesLoader = PackageType.XETYooAssets * 1000 + 2;
-    }
-}
