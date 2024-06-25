233515b93672f32341f70ebd175243fd7c7a26bf
IcePower <icepower@git.com>
2024-06-25 16:45:07 +0800

    init

---
+++ Scripts/Model/Share/CoroutineLockType.cs
@@ -0,0 +1,8 @@
+namespace ET
+{
+    public static partial class CoroutineLockType
+    {
+        public const int Resources = PackageType.XETYooAssets * 1000 + 1;
+        public const int ResourcesLoader = PackageType.XETYooAssets * 1000 + 2;
+    }
+}
